3a38c9441ee8622379c6c731903a1be168c8594e
mw332637 <mw332637@students.mimuw.edu.pl>
2016-12-07 23:14:00 +0100

    Added my strategy
    We only need some documentation now.

---
+++ R/strategy_EV.R
@@ -0,0 +1,15 @@
+#' @title Strategia Expected Value
+#' 
+#' @description Strategia polega na wybraniu ruchu, dla ktorego wartosc
+#'  oczekiwana liczona w krolikach jest najwieksza. Funkcja \code{moves_generation()} 
+#'  generuje wszystkie mozliwe ruchy. Funkcja \code{get_the_best_state} zwraca ruch o 
+#'  największej 
+strategy_EV <- function (stock_status) {
+  animals_in_necessary_order <- c("kon", "krowa", "duzy_pies", "swinia", "owca", "maly_pies", "krolik")
+  stock_status <- stock_status[animals_in_necessary_order]
+  possible_moves <- moves_generation(stock_status)
+  possible_states <- t(t(possible_moves) + stock_status)
+  colnames(possible_states) <- animals_in_necessary_order
+  animals_in_correct_order <- c("krolik", "owca", "swinia", "krowa", "kon", "maly_pies", "duzy_pies")
+  return (get_the_best_state(possible_states)[animals_in_correct_order])
+}
