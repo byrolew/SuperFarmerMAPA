df6527ad2f5ca650e71a508554341e4351de0695
mw332637 <mw332637@students.mimuw.edu.pl>
2016-12-08 12:08:46 +0100

    UPdated strategy_ev

--- R/strategy_EV.R
+++ R/strategy_EV.R
@@ -9,9 +9,10 @@
 strategy_EV <- function (stock_status) {
   animals_in_necessary_order <- c("kon", "krowa", "duzy_pies", "swinia", "owca", "maly_pies", "krolik")
   stock_status <- stock_status[animals_in_necessary_order]
-  possible_moves <- moves_generation(stock_status)
+  possible_moves <- rbind(moves_generation(stock_status), c(0, 0, 0, 0, 0, 0, 0))
   possible_states <- t(t(possible_moves) + stock_status)
   colnames(possible_states) <- animals_in_necessary_order
   animals_in_correct_order <- c("krolik", "owca", "swinia", "krowa", "kon", "maly_pies", "duzy_pies")
+  print(get_the_best_state(possible_states)[animals_in_correct_order])
   return (get_the_best_state(possible_states)[animals_in_correct_order])
 }
