28a32d632d441fc1cb7c16c565ec6f3a6090f2bf
kot <agnieszkac27@gmail.com>
2017-01-18 19:28:35 +0100

    Dwie pierwsze wizytowki, bez rf

--- R/wizytowka_KukurydzaNaPizzy.R
+++ R/wizytowka_KukurydzaNaPizzy.R
@@ -1,6 +1,14 @@
 #' @title Wizytowka
 #' 
+#' @description Funkcja \code{wizytowka_KukurydzaNaPizzy} generuje wizytowke wybranej 
+#' strategii w formacie pdf
+#' 
+#' @param strategia Strategia, dla ktorej na byc wygenerowana wizytowka
+#' 
+#' @return Wizytowka.pdf Wizytowka, pokazujaca statystyki na temat strategii oraz jej dzialanie
+#' 
 #' @rdname wizytowka_KukurydzaNaPizzy
+#' 
 #' @export
 
 wizytowka_KukurydzaNaPizzy <- function(strategia){
@@ -12,7 +20,7 @@ wizytowka_KukurydzaNaPizzy <- function(strategia){
   )
   
   
-  lista <- SuperFarmerMAPA::badaj_gre(strategia, 100)
+  lista <- SuperFarmerMAPA::badaj_gre(strategia)
   dane <- unlist(lista[[1]])
   m <- lista[[2]]
   lay <- rbind(c(1,1),
@@ -27,25 +35,25 @@ wizytowka_KukurydzaNaPizzy <- function(strategia){
                c(4,4),
                c(6,6))
   
-  tytul <- textGrob(gsub("::","\n",deparse(substitute(strategia))), gp=gpar(fontsize=30))
+  tytul <- grid::textGrob(gsub("::","\n",deparse(substitute(strategia))), gp=grid::gpar(fontsize=30))
   stat <-unclass(summary(dane))
-  names(stat) <- c("Min.", "1szy kw.", "Mediana", "Średnia", "3ci kw.", "Max.")
+  names(stat) <- c("Min.", "1szy kw.", "Mediana", "Srednia", "3ci kw.", "Max.")
   statystyki <- t(data.frame(stat, check.names = FALSE, stringsAsFactors = FALSE))
   rownames(statystyki) <- NULL
-  p1 <-tableGrob(statystyki,theme = tt1)
+  p1 <-gridExtra::tableGrob(statystyki,theme = tt1)
   p2 <- rysuj_gestosc(dane)
   #tutaj zapisujemy plik
   png('rplot.png',width = 1280, height = 1280, res = 200)
   kolowy(m)
   dev.off()
   # tu go wczytujemy 
-  p3_tytul <- textGrob("        Przeplyw wartosci w krolikach w wymianach", gp=gpar(fontsize=13), just = "right")
+  p3_tytul <- grid::textGrob("        Przeplyw wartosci w krolikach w wymianach", gp=grid::gpar(fontsize=13), just = "right")
   p3 <- rasterGrob(readPNG('rplot.png'))
   #######TO zapisywanie i wczytywanie pliku jest baaaaaaaaaaaaardzo nieeleganckie!!!!!
   
-  p_podpis <- textGrob("grupa KukurydzaNaPizzy", gp=gpar(fontsize=8), just = "left")
+  p_podpis <- grid::textGrob("grupa KukurydzaNaPizzy", gp=grid::gpar(fontsize=8), just = "left")
   
-  pdf("Wizytowka_test.pdf",width=8.27, height=11.69)
+  pdf("Wizytowka_rf.pdf",width=8.27, height=11.69)
   grid.arrange(tytul,p1,p2,p3,p3_tytul,p_podpis,layout_matrix = lay)
   dev.off()
 }
