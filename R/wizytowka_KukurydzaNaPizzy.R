82d3febef030cbcf2598955dacb76f765e3cd0b6
hansiu <unchallenged.yo@gmail.com>
2017-01-19 16:42:42 +0100

    Dodana zmiana nazwy wizytowki - niestestowana

--- R/wizytowka_KukurydzaNaPizzy.R
+++ R/wizytowka_KukurydzaNaPizzy.R
@@ -1,25 +1,25 @@
 #' @title Wizytowka
-#' 
-#' @description Funkcja \code{wizytowka_KukurydzaNaPizzy} generuje wizytowke wybranej 
+#'
+#' @description Funkcja \code{wizytowka_KukurydzaNaPizzy} generuje wizytowke wybranej
 #' strategii w formacie pdf
-#' 
+#'
 #' @param strategia Strategia, dla ktorej na byc wygenerowana wizytowka
-#' 
+#'
 #' @return Wizytowka.pdf Wizytowka, pokazujaca statystyki na temat strategii oraz jej dzialanie
-#' 
+#'
 #' @rdname wizytowka_KukurydzaNaPizzy
-#' 
+#'
 #' @export
 
 wizytowka_KukurydzaNaPizzy <- function(strategia){
-  
+
   tt1 <- gridExtra::ttheme_default(
     core = list(bg_params=list(fill=c("aliceblue", "pink"))),
     colhead = list(fg_params=list(col="white"),
                    bg_params=list(fill="cadetblue3"))
   )
-  
-  
+
+
   lista <- SuperFarmerMAPA::badaj_gre(strategia)
   dane <- unlist(lista[[1]])
   m <- lista[[2]]
@@ -34,7 +34,7 @@ wizytowka_KukurydzaNaPizzy <- function(strategia){
                c(4,4),
                c(4,4),
                c(6,6))
-  
+
   tytul <- grid::textGrob(gsub("::","\n",deparse(substitute(strategia))), gp=grid::gpar(fontsize=30))
   stat <-unclass(summary(dane))
   names(stat) <- c("Min.", "1szy kw.", "Mediana", "Srednia", "3ci kw.", "Max.")
@@ -46,14 +46,14 @@ wizytowka_KukurydzaNaPizzy <- function(strategia){
   png('rplot.png',width = 1280, height = 1280, res = 200)
   kolowy(m)
   dev.off()
-  # tu go wczytujemy 
+  # tu go wczytujemy
   p3_tytul <- grid::textGrob("        Przeplyw wartosci w krolikach w wymianach", gp=grid::gpar(fontsize=13), just = "right")
   p3 <- rasterGrob(readPNG('rplot.png'))
   #######TO zapisywanie i wczytywanie pliku jest baaaaaaaaaaaaardzo nieeleganckie!!!!!
-  
+
   p_podpis <- grid::textGrob("grupa KukurydzaNaPizzy", gp=grid::gpar(fontsize=8), just = "left")
-  
-  pdf("Wizytowka_rf.pdf",width=8.27, height=11.69)
+  nazwa_pliku <- paste0("Wizytowka_",gsub('\"',"",gsub("::","_",gsub("\\.","",deparse(substitute("SuperFarmer.SuperDziewczyn::strategia_owce"))))),".pdf")
+  pdf(nazwa_pliku,width=8.27, height=11.69)
   grid.arrange(tytul,p1,p2,p3,p3_tytul,p_podpis,layout_matrix = lay)
   dev.off()
 }
