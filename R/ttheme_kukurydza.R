5af201ed3b3fdda61e13edaad73bef8608a2acfa
kot <agnieszkac27@gmail.com>
2017-01-24 10:03:10 +0100

    Zmiana kolorow i dodanie folderu inst

--- R/ttheme_kukurydza.R
+++ R/ttheme_kukurydza.R
@@ -2,19 +2,20 @@
 #'
 #' @description Funkcja \code{ttheme_kukurydza} generuje ttheme do tabeli statystyk
 #'
-#'
 #' @return tt1 szablon wygladu
 #'
 #' @import gridExtra
 #'
 #' @export
-#'
+
 ttheme_kukurydza <- function(){
   tt1 <- gridExtra::ttheme_default(
     core = list(fg_params = list(col="white"),
-                bg_params=list(fill=c("firebrick1", "gray7", "green3"))),
-    colhead = list(fg_params=list(col="white"),
-                   bg_params=list(fill="cadetblue3")),
+                bg_params=list(fill=c(rgb(230, 90, 90, maxColorValue = 255), 
+                                      rgb(50, 50, 50, maxColorValue = 255), 
+                                      rgb(90, 230, 110, maxColorValue = 255)))),
+    colhead = list(fg_params=list(col="black"),
+                   bg_params=list(fill=rgb(220, 220, 220, maxColorValue = 255))),
     rowhead = list(fg_params=list(cex = 1.0)))
   return(tt1)
 }
--- R/wizytowka_KukurydzaNaPizzy.R
+++ R/wizytowka_KukurydzaNaPizzy.R
@@ -30,29 +30,34 @@ wizytowka_KukurydzaNaPizzy <- function(strategia,powtorz=10000){
                c(7,6))
 
   tytul <- grid::textGrob(gsub("::","\n",deparse(substitute(strategia))), gp=grid::gpar(fontsize=30))
-  p1 <-gridExtra::tableGrob(staty(dane, strsplit(deparse(substitute(strategia)),"::")[[1]][2]),theme = ttheme_kukurydza())
+  if("::" %in% deparse(substitute(strategia))){
+    nazwa <- strsplit(deparse(substitute(strategia)),"::")[[1]][2]
+  } else {
+    nazwa <- deparse(substitute(strategia))
+  }
+  p1 <-gridExtra::tableGrob(staty(dane, nazwa),theme = ttheme_kukurydza())
   p2 <- rysuj_gestosc(dane)
   #tutaj zapisujemy plik
   png('rplot.png',width = 1280, height = 1280, res = 200)
   kolowy(m)
   dev.off()
   # tu go wczytujemy
-  p3_tytul <- grid::textGrob("Przepływ wartosci w krolikach w wymianach", gp=grid::gpar(fontsize=13), just = "center")
+  p3_tytul <- grid::textGrob("Przeplyw wartosci w krolikach w wymianach", gp=grid::gpar(fontsize=13), just = "center")
    p3 <- rasterGrob(readPNG('rplot.png'))
   #######TO zapisywanie i wczytywanie pliku jest baaaaaaaaaaaaardzo nieeleganckie!!!!!
-  p3_opis <- grid::textGrob("Wykres wizualizuje wymiany dokonywane przez strategię.\n
-Jednostką wykresu jest wartość jednego królika.\n
-Długość wycinka odpowiadającego zwierzęciu pokazuje udział jego wartości w we wszystkich\n
-wymianach (cały okrąg).\n
-Przepływy między wycinkami pokazują, na co i jak wartościowe były wymiany zwierzęcia\n
+  p3_opis <- grid::textGrob("Wykres wizualizuje wymiany dokonywane przez strategie.\n
+Jednostka wykresu jest wartosc jednego krolika.\n
+Dlugosc wycinka odpowiadajacego zwierzeciu pokazuje udzial jego wartosci w we wszystkich\n
+wymianach (caly okrag).\n
+Przeplywy miedzy wycinkami pokazuja, na co i jak wartosciowe byly wymiany zwierzecia\n
 o odpowiednim kolorze.",
                             gp=grid::gpar(fontsize=10, lineheight	= 0.5),
                             x = unit(0, "npc"),
                             y = unit(0.7, "npc"),
                             just = "left")
   p_podpis <- grid::textGrob("grupa KukurydzaNaPizzy", gp=grid::gpar(fontsize=8), just = "left")
-  nazwa_pliku <- paste0("Wizytowka_",gsub('\"',"",gsub("::","_",gsub("\\.","",deparse(substitute(strategia))))),".pdf")
-  pdf(nazwa_pliku,width=11.69, height=8.27)
+  nazwa_pliku <- paste0("KukurydzaNaPizzy_Wizytowka_",gsub('\"',"",gsub("::","_",gsub("\\.","",deparse(substitute(strategia))))),".pdf")
+  pdf(paste0("inst/",nazwa_pliku),width=11.69, height=8.27)
   grid.arrange(tytul,p3,p2,p1,p3_tytul,p3_opis,p_podpis,layout_matrix = lay)
   dev.off()
 }
